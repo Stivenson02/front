c428cde9edacabf1128f30423feb27102711354a
ismael <ismael.gomez@brm.com.co>
2019-01-16 11:23:03 -0500

    are change route reg_aleatorio to gestion-manual are create button for formulario gestion-manual

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
           { path: 'bs-element', loadChildren: './bs-element/bs-element.module#BsElementModule' },
           { path: 'grid', loadChildren: './grid/grid.module#GridModule' },
           { path: 'components', loadChildren: './bs-component/bs-component.module#BsComponentModule' },
-          { path: 'reg_aleatorio', loadChildren: './reg_aleatorio/reg_aleatorio.module#Reg_AleatorioModule'},
+          { path: 'gestion-manual', loadChildren: './reg_aleatorio/reg_aleatorio.module#Reg_AleatorioModule'},
           { path: 'blank-page', loadChildren: './blank-page/blank-page.module#BlankPageModule' }
         ]
       }
