99890413eb3b02bb76f7b7a067f131a7c5bac978
ismael <ismael.gomez@brm.com.co>
2019-01-22 09:35:13 -0500

    edit locate of reg_aleatorio folder

--- src/app/layout/telemercadeo/reg_aleatorio/reg_aleatorio.module.ts
+++ src/app/layout/telemercadeo/reg_aleatorio/reg_aleatorio.module.ts
@@ -5,7 +5,7 @@ import { CommonModule } from '@angular/common';
 
 import { Reg_AleatorioRoutingModule } from './reg_aleatorio-routing.module';
 import { Reg_AleatorioComponent } from './reg_aleatorio.component';
-import { PageHeaderModule } from '../../shared';
+import { PageHeaderModule } from '../../../shared';
 
 import { MatFormFieldModule} from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
