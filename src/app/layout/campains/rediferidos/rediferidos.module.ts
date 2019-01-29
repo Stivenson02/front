f5d0ab0d095b370488e5af7da128cbdfcdb869bc
Stiven <stiven.maldonado@brm.com.co>
2019-01-29 16:55:09 -0500

    table guion

--- src/app/layout/campains/rediferidos/rediferidos.module.ts
+++ src/app/layout/campains/rediferidos/rediferidos.module.ts
@@ -1,33 +1,20 @@
-
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbCarouselModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule , ReactiveFormsModule } from '@angular/forms';
+import { FormsModule} from '@angular/forms';
 import { RediferidosRoutingModule } from './rediferidos-routing.module';
-import { environment } from '../../../../environments/environment';
-
-
-
-import { StatModule } from '../../../shared/modules';
 import {
-  AccordionModule, ChartModule, DialogModule, InputTextareaModule,
-  InputTextModule, ListboxModule, MessageModule, MessagesModule,
-  ScrollPanelModule,
-  SplitButtonModule,
-  TabMenuModule,
-  TabViewModule
+  InputTextModule, MessageModule, MessagesModule,
+
 } from 'primeng/primeng';
 
 import { RediferidosComponent } from './rediferidos.component';
 
 import {
-   Guion1Component
+  Guion1Component
 } from './components';
 import {ToastModule} from 'primeng/toast';
-import {MatNativeDateModule, MatTableModule} from '@angular/material';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatInputModule} from '@angular/material';
+
 import {PreguionComponent} from '../components';
 
 @NgModule({
@@ -36,27 +23,11 @@ import {PreguionComponent} from '../components';
     NgbCarouselModule,
     NgbAlertModule,
     RediferidosRoutingModule,
-    StatModule,
     FormsModule,
-    ReactiveFormsModule,
-    AccordionModule,
     InputTextModule,
-    TabMenuModule,
-    TabViewModule,
-    ScrollPanelModule,
-    SplitButtonModule,
     MessagesModule,
     MessageModule,
     ToastModule,
-    DialogModule,
-    ListboxModule,
-    InputTextareaModule,
-    MatNativeDateModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatFormFieldModule,
-    MatInputModule,
-    ChartModule
   ],
   declarations: [
     RediferidosComponent,
