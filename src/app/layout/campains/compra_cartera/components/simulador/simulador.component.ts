ecefa357e423d4a16c53dc4359033760d0f84696
ismael <ismael.gomez@brm.com.co>
2019-01-30 12:15:23 -0500

    edit table of simulador component within compra-cartera module

--- src/app/layout/campains/compra_cartera/components/simulador/simulador.component.ts
+++ src/app/layout/campains/compra_cartera/components/simulador/simulador.component.ts
@@ -9,17 +9,49 @@ import {MessageService} from 'primeng/api';
 })
 export class SimuladorComponent implements OnInit {
 
-  numeroCuota: number[];
-	showTableDavi: boolean = false;
-  interesOtro: number = 0;
-
-  constructor() {}
   
+  interesDavi: number = 1;
+  totalCuotasDavi: number = 3;
+  
+  interesOtra: number = 6;
+  totalCuotasOtra: number = 8;
+
+  totalSaldo: number = 100;
+
+  total: number;
+  
+  numeroCuotaDavi: number[];
+	saldoDavi: number[];
+  abonoCapitalDavi: number[];
+  abonoInteresDavi: number[];
+  pagoMinimoDavi: number[];
+
+  numeroCuotaOtra: number[];
+  saldoOtra: number[];
+  abonoCapitalOtra: number[];
+  abonoInteresOtra: number[];
+  pagoMinimoOtra: number[];
+
+  showTableDavi: boolean = false;
+  
+  constructor() {
+    //this.numeroCuotaDavi[0] = 1;
+    //this.numeroCuotaDavi[1] = 2;
+    //this.saldoDavi[0] = this.totalSaldo;
+    //this.abonoCapitalDavi[0] = this.totalSaldo / this.totalCuotasDavi;
+    //this.abonoInteresDavi[0] = this.totalSaldo * this.interesDavi;
+    //this.pagoMinimoDavi[0] = this.abonoCapitalDavi[0] + this.abonoInteresDavi[0];
+  }
+
   showHiddenTable(){
   	this.showTableDavi = true;
   }
 
-  ngOnInit() {
+  onInputFunction(){
     
+  }
+
+  ngOnInit() {
+   
   }
 }
